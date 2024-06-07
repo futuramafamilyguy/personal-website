63297b8d5e4038688f56ac5c93a8fc2579295e15
unknown <awan968@aucklanduni.ac.nz>
2024-06-07 17:39:25 +1200

    configured mongodb

--- server/src/PersonalWebsite.Api/Program.cs
+++ server/src/PersonalWebsite.Api/Program.cs
@@ -1,6 +1,9 @@
+using PersonalWebsite.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddMongoClient(builder.Configuration.GetConnectionString("PersonalWebsiteDb"));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ server/src/PersonalWebsite.Infrastructure/ServiceExtensions.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
+
+namespace PersonalWebsite.Infrastructure;
+
+public static class ServiceExtensions
+{
+    public static void AddMongoClient(this IServiceCollection services, string connectionString)
+    {
+        services.AddSingleton<IMongoClient, MongoClient>(sp => new MongoClient(connectionString));
+    }
+}
