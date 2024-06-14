4a06a97536b46c1a04f02fbbeb5cdaedddf08650
unknown <awan968@aucklanduni.ac.nz>
2024-06-15 01:46:21 +1200

    enabled cors

--- server/src/PersonalWebsite.Api/Program.cs
+++ server/src/PersonalWebsite.Api/Program.cs
@@ -18,6 +18,16 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(
+        "AllowAll",
+        policy =>
+            policy.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+    );
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -32,5 +42,7 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseCors("AllowAll");
 
 app.Run();
