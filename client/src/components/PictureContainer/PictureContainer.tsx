3fbdcc67944f414d888f3ffcc3fe9f6c1440b3bf
unknown <awan968@aucklanduni.ac.nz>
2024-06-16 11:06:15 +1200

    implemented loading component

--- client/src/components/PictureContainer/PictureContainer.tsx
+++ client/src/components/PictureContainer/PictureContainer.tsx
@@ -1,5 +1,6 @@
 import usePictures from "../..//hooks/usePictures";
 import CapsuleButton from "../CapsuleButton/CapsuleButton";
+import Loading from "../Loading/Loading";
 import MediaCard from "../MediaCard/MediaCard";
 import Pagination from "../Pagination/Pagination";
 import styles from "./PictureContainer.module.css";
@@ -18,8 +19,6 @@ const PictureContainer: React.FC = () => {
 
   const years = [2024, 2023, 2022];
 
-  if (loading) return <p>Loading...</p>;
-
   return (
     <>
       <div className={styles.activeYearsArea}>
@@ -33,28 +32,35 @@ const PictureContainer: React.FC = () => {
           />
         ))}
       </div>
-      <div className={styles.pictureContainer}>
-        {pictures.map((picture) => (
-          <MediaCard
-            key={picture.id}
-            imageUrl={
-              picture.imageUrl ??
-              "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/64d42ace-e41a-42da-9370-a6623fa7471b/11312ad4-f5ca-417a-a411-87422e0c4f5e?host=wbd-images.prod-vod.h264.io&partner=beamcom"
-            }
-            title={picture.alias ?? picture.name}
-          />
-        ))}
-        {currentPage === totalPages &&
-          emptyMediaCardArray.map((index) => (
-            <div className={styles.emptyMediaCard} key={index}></div>
-          ))}
-      </div>
 
-      <Pagination
-        currentPage={currentPage}
-        totalPages={totalPages}
-        onPageChange={setCurrentPage}
-      />
+      {loading ? (
+        <Loading />
+      ) : (
+        <>
+          <div className={styles.pictureContainer}>
+            {pictures.map((picture) => (
+              <MediaCard
+                key={picture.id}
+                imageUrl={
+                  picture.imageUrl ??
+                  "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/64d42ace-e41a-42da-9370-a6623fa7471b/11312ad4-f5ca-417a-a411-87422e0c4f5e?host=wbd-images.prod-vod.h264.io&partner=beamcom"
+                }
+                title={picture.alias ?? picture.name}
+              />
+            ))}
+            {currentPage === totalPages &&
+              emptyMediaCardArray.map((index) => (
+                <div className={styles.emptyMediaCard} key={index}></div>
+              ))}
+          </div>
+
+          <Pagination
+            currentPage={currentPage}
+            totalPages={totalPages}
+            onPageChange={setCurrentPage}
+          />
+        </>
+      )}
     </>
   );
 };
