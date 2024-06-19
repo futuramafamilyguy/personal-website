88bd047189fd95af3a092789dd2fc95cedef9a8a
unknown <awan968@aucklanduni.ac.nz>
2024-06-19 13:42:00 +1200

    implemented custom message display for empty picture fetch reuslts

--- client/src/components/PictureContainer/PictureContainer.tsx
+++ client/src/components/PictureContainer/PictureContainer.tsx
@@ -1,8 +1,9 @@
 import usePictures from "../..//hooks/usePictures";
-import Loading from "../Loading/Loading";
+import MessageDisplay from "../MessageDisplay/MessageDisplay";
 import MediaCard from "../MediaCard/MediaCard";
 import Pagination from "../Pagination/Pagination";
 import styles from "./PictureContainer.module.css";
+import megamind from "../../assets/megamind.png";
 
 const PictureContainer: React.FC = () => {
   const {
@@ -14,38 +15,49 @@ const PictureContainer: React.FC = () => {
     setCurrentPage,
   } = usePictures(1);
 
-  return (
-    <>
-      {loading ? (
-        <Loading />
-      ) : (
-        <>
-          <div className={styles.pictureContainer}>
-            {pictures.map((picture) => (
-              <MediaCard
-                key={picture.id}
-                imageUrl={
-                  picture.imageUrl ??
-                  "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/64d42ace-e41a-42da-9370-a6623fa7471b/11312ad4-f5ca-417a-a411-87422e0c4f5e?host=wbd-images.prod-vod.h264.io&partner=beamcom"
-                }
-                title={picture.alias ?? picture.name}
-              />
-            ))}
-            {currentPage === totalPages &&
-              emptyMediaCardArray.map((index) => (
-                <div className={styles.emptyMediaCard} key={index}></div>
+  const renderContent = () => {
+    if (loading) {
+      return <MessageDisplay message={"Loading..."} />;
+    } else {
+      if (pictures.length > 0) {
+        return (
+          <>
+            <div className={styles.pictureContainer}>
+              {pictures.map((picture) => (
+                <MediaCard
+                  key={picture.id}
+                  imageUrl={
+                    picture.imageUrl ??
+                    "https://beam-images.warnermediacdn.com/BEAM_LWM_DELIVERABLES/64d42ace-e41a-42da-9370-a6623fa7471b/11312ad4-f5ca-417a-a411-87422e0c4f5e?host=wbd-images.prod-vod.h264.io&partner=beamcom"
+                  }
+                  title={picture.alias ?? picture.name}
+                />
               ))}
-          </div>
+              {currentPage === totalPages &&
+                emptyMediaCardArray.map((index) => (
+                  <div className={styles.emptyMediaCard} key={index}></div>
+                ))}
+            </div>
 
-          <Pagination
-            currentPage={currentPage}
-            totalPages={totalPages}
-            onPageChange={setCurrentPage}
-          />
-        </>
-      )}
-    </>
-  );
+            <Pagination
+              currentPage={currentPage}
+              totalPages={totalPages}
+              onPageChange={setCurrentPage}
+            />
+          </>
+        );
+      } else {
+        return (
+          <MessageDisplay
+            message={"no pictures?"}
+            imageUrl={megamind}
+          ></MessageDisplay>
+        );
+      }
+    }
+  };
+
+  return renderContent();
 };
 
 export default PictureContainer;
