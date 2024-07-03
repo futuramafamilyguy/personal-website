12831d3aa8495507d6de9921acc02bf40d054c26
unknown <awan968@aucklanduni.ac.nz>
2024-07-03 18:50:14 +1200

    added GET endpoint for fetching visit stats

---
+++ server/src/PersonalWebsite.Api/Controllers/StatisticsController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonalWebsite.Infrastructure.Data.Visits;
+
+namespace PersonalWebsite.Api.Controllers;
+
+[ApiController]
+[Route("stats")]
+public class StatisticsController : ControllerBase
+{
+    private readonly VisitStatisticsRepository _visitStatisticsRepository;
+
+    public StatisticsController(VisitStatisticsRepository visitStatisticsRepository)
+    {
+        _visitStatisticsRepository = visitStatisticsRepository;
+    }
+
+    [HttpGet("")]
+    public async Task<ActionResult> GetCinemasAsync()
+    {
+        var stats = await _visitStatisticsRepository.GetVisitStatisticsAsync();
+
+        return Ok(stats);
+    }
+}
