cfea872a54eeb46c878ae08fc8992733fca08aa2
unknown <awan968@aucklanduni.ac.nz>
2024-11-02 01:48:22 +1300

    fixed bug where segoe ui wasnt being rendered in modals on macos

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -7,12 +7,14 @@ import App from "./App.tsx";
 const isMacOs = () => {
   const platform = window.navigator.platform;
   return ["Macintosh", "MacIntel", "MacPPC", "Mac68K"].includes(platform);
-}
+};
 
 const root = document.getElementById("root")!;
+const portal = document.getElementById("portal")!;
 
 if (isMacOs()) {
   root.style.fontFamily = "'Segoe UI', sans-serif";
+  portal.style.fontFamily = "'Segoe UI', sans-serif";
 }
 
 ReactDOM.createRoot(root!).render(
