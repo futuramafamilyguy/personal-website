0bfeb314ebc52d2686bed47b8d1702efce5846c6
unknown <awan968@aucklanduni.ac.nz>
2025-01-24 23:26:28 +1300

    created custom image for nginx

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -5,16 +5,14 @@ KEY=/etc/nginx/ssl/live/$DOMAIN/privkey.pem
 
 if [[ -f /$CERT && -f $KEY ]]; then
 	echo 'loading live config'
-	envsubst '${SERVER_URL} ${CLIENT_URL} ${DOMAIN} ${SESSION_HUB_URL}' < /etc/nginx/conf.d/default.template > /etc/nginx/conf.d/default.conf
+	envsubst '${SERVER_URL} ${CLIENT_URL} ${DOMAIN} ${SESSION_HUB_URL}' < /templates/default.conf.template > /etc/nginx/conf.d/default.conf
 	if [ -f /etc/nginx/conf.d/certbot.conf ]; then
 		rm /etc/nginx/conf.d/certbot.conf
 	fi
 else
 	echo 'loading http-01 challenge config'
-	envsubst '${DOMAIN}' < /etc/nginx/conf.d/certbot.template > /etc/nginx/conf.d/certbot.conf
+	envsubst '${DOMAIN}' < /templates/certbot.conf.template > /etc/nginx/conf.d/certbot.conf
 	if [ -f /etc/nginx/conf.d/default.conf ]; then
 		rm /etc/nginx/conf.d/default.conf
 	fi
 fi
-
-nginx -g 'daemon off;'
