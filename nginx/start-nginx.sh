b7af02a97eccc4f56c386368718cfdde307684a7
unknown <awan968@aucklanduni.ac.nz>
2025-01-28 10:58:09 +1300

    fixed whitespace in scripts and nginx config

--- nginx/start-nginx.sh
+++ nginx/start-nginx.sh
@@ -4,15 +4,15 @@ CERT=/etc/nginx/ssl/live/$DOMAIN/fullchain.pem
 KEY=/etc/nginx/ssl/live/$DOMAIN/privkey.pem
 
 if [[ -f /$CERT && -f $KEY ]]; then
-	echo 'loading live config'
-	envsubst '${SERVER_URL} ${CLIENT_URL} ${DOMAIN} ${SESSION_HUB_URL}' < /templates/default.conf.template > /etc/nginx/conf.d/default.conf
-	if [ -f /etc/nginx/conf.d/certbot.conf ]; then
-		rm /etc/nginx/conf.d/certbot.conf
-	fi
+    echo 'loading live config'
+    envsubst '${SERVER_URL} ${CLIENT_URL} ${DOMAIN} ${SESSION_HUB_URL}' < /templates/default.conf.template > /etc/nginx/conf.d/default.conf
+    if [ -f /etc/nginx/conf.d/certbot.conf ]; then
+        rm /etc/nginx/conf.d/certbot.conf
+    fi
 else
-	echo 'loading http-01 challenge config'
-	envsubst '${DOMAIN}' < /templates/certbot.conf.template > /etc/nginx/conf.d/certbot.conf
-	if [ -f /etc/nginx/conf.d/default.conf ]; then
-		rm /etc/nginx/conf.d/default.conf
-	fi
+    echo 'loading http-01 challenge config'
+    envsubst '${DOMAIN}' < /templates/certbot.conf.template > /etc/nginx/conf.d/certbot.conf
+    if [ -f /etc/nginx/conf.d/default.conf ]; then
+        rm /etc/nginx/conf.d/default.conf
+    fi
 fi
