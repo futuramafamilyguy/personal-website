bd3d7b760e5265796307521f5076d1b49499bd95
unknown <awan968@aucklanduni.ac.nz>
2025-02-15 22:45:37 +1300

    mention lucy lawless is xena: warrior princess

--- client/src/pages/AboutPage/AboutPage.tsx
+++ client/src/pages/AboutPage/AboutPage.tsx
@@ -8,7 +8,9 @@ function AboutPage() {
           <h3>kia ora *~*</h3>
           <h5>
             I'm a frequent moviegoer based in South Auckland, New Zealand—home
-            of Big Ben Pies and Lucy Lawless. Thanks for visiting *~*
+            of Big Ben Pies and Lucy Lawless, aka Xena: Warrior Princess{" "}
+            <span className={styles.smallText}>i love u lucy</span> Thanks for
+            visiting *~*
           </h5>
           <h5>
             <i>life's hard but rewa's harder</i>
