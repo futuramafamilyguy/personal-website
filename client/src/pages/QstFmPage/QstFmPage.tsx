7634929471a587f34d0f4d0ec4257886262b7906
unknown <awan968@aucklanduni.ac.nz>
2025-11-12 23:47:44 +1300

    parameterised QST FM page content by radio segment

--- client/src/pages/QstFmPage/QstFmPage.tsx
+++ client/src/pages/QstFmPage/QstFmPage.tsx
@@ -1,12 +1,42 @@
+import { useEffect, useState } from "react";
+
+import QstFm from "../../components/QstFm/QstFm";
 import styles from "./QstFmPage.module.css";
 
+interface Track {
+  title: string;
+  artist: string;
+  url: string;
+  original: string;
+}
+
+interface Segment {
+  cover: string;
+  intro: string;
+  playlist: Track[];
+}
+
+const turtleDoves: Segment = {
+  cover: "https://cdn.allenmaygibson.com/images/static/turtle-doves.jpg",
+  intro:
+    "good evening, queen street. this next one goes out to our loves — the ones we fight with, the ones we dance with, and the ones we still miss. ohia noa atu. my longing for you is unrestricted",
+  playlist: [],
+};
+
 function QstFmPage() {
+  const [segment, setSegment] = useState<Segment>(turtleDoves);
+
   return (
-    <div className={styles.qstFmPage}>
+    <div
+      className={styles.qstFmPage}
+      style={{
+        ["--bg-image" as any]: `url(${segment.cover})`,
+      }}
+    >
       <div className={styles.centeredContainer}>
         <div className={styles.descriptionBox}>
           <h5>
-            <i>temp intro</i>
+            <i>{segment.intro}</i>
           </h5>
         </div>
       </div>
