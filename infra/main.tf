897c83f15a40ade00b204ff022ae1a63bb894fd1
unknown <awan968@aucklanduni.ac.nz>
2026-03-04 23:02:34 +1300

    add terraform config for lightsail server container

--- infra/main.tf
+++ infra/main.tf
@@ -19,6 +19,11 @@ provider "aws" {
   region = "ap-southeast-2"
 }
 
+variable "domain_name" {
+  type    = string
+  default = "allenmaygibson.com"
+}
+
 resource "aws_lightsail_container_service" "personal_website_client_service" {
   name  = "personal-website-client-service"
   power = "small"
@@ -29,6 +34,7 @@ resource "aws_lightsail_container_service_deployment_version" "personal_website_
   container {
     container_name = "client"
     image          = "ghcr.io/futuramafamilyguy/personal-website-client:latest"
+
     ports = {
       3000 = "HTTP"
     }
@@ -46,4 +52,65 @@ resource "aws_lightsail_container_service_deployment_version" "personal_website_
   }
 
   service_name = aws_lightsail_container_service.personal_website_client_service.name
+}
+
+resource "aws_secretsmanager_secret" "personal_website" {
+  name = "personal-website"
+}
+
+data "aws_secretsmanager_secret_version" "personal_website_value" {
+  secret_id = aws_secretsmanager_secret.personal_website.id
+}
+
+locals {
+  secrets = jsondecode(data.aws_secretsmanager_secret_version.personal_website_value.secret_string)
+}
+
+resource "aws_lightsail_container_service" "personal_website_server_service" {
+  name  = "personal-website-server-service"
+  power = "small"
+  scale = 1
+}
+
+resource "aws_lightsail_container_service_deployment_version" "personal_website_server_deployment" {
+  container {
+    container_name = "server"
+    image          = "ghcr.io/futuramafamilyguy/personal-website-server:latest"
+
+    environment = {
+      ImageStorageConfiguration__Provider      = "S3"
+      ImageStorageConfiguration__Host          = "https://s3.ap-southeast-2.amazonaws.com/"
+      ImageStorageConfiguration__CdnEnabled    = "true"
+      MarkdownStorageConfiguration__Provider   = "S3"
+      MarkdownStorageConfiguration__Host       = "https://s3.ap-southeast-2.amazonaws.com/"
+      MarkdownStorageConfiguration__CdnEnabled = "true"
+      AllowedOrigins                           = "https://${var.domain_name},https://www.${var.domain_name}"
+      CdnConfiguration__Host                   = "https://cdn.${var.domain_name}"
+      ConnectionStrings__PersonalWebsiteDb     = local.secrets["ConnectionStrings__PersonalWebsiteDb"]
+      AWS_ACCESS_KEY_ID                        = local.secrets["AWS_ACCESS_KEY_ID"]
+      AWS_SECRET_ACCESS_KEY                    = local.secrets["AWS_SECRET_ACCESS_KEY"]
+      AWS_REGION                               = "ap-southeast-2"
+      S3Configuration__BucketName              = local.secrets["S3Configuration__BucketName"]
+      BasicAuthConfiguration__AdminUsername    = local.secrets["BasicAuthConfiguration__AdminUsername"]
+      BasicAuthConfiguration__AdminPassword    = local.secrets["BasicAuthConfiguration__AdminPassword"]
+    }
+
+    ports = {
+      8080 = "HTTP"
+    }
+  }
+
+  public_endpoint {
+    container_name = "server"
+    container_port = 8080
+
+    health_check {
+      unhealthy_threshold = 3
+      timeout_seconds     = 5
+      interval_seconds    = 30
+      path                = "/health"
+    }
+  }
+
+  service_name = aws_lightsail_container_service.personal_website_server_service.name
 }
